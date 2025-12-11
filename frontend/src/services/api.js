284cdbc250dbe7026397a46a78488153c26e313a
Devendrnagar <devendrnagar853@gmail.com>
2025-12-11 09:54:29 +0530

    frontend add a backend url commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://login-register-full-1.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
